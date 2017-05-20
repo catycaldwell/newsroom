cc9e8b764299093d2fe4317fddc90d0630816086
Taylor Krusen <taykrusen@gmail.com>
2017-05-20 13:09:26 -0700

    basic project structure... messy

---
+++ client/app.js
@@ -0,0 +1,52 @@
+var app = angular.module('app', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'partials/home.html',
+    })
+    // .when('/dash', {
+    //     templateUrl: 'partials/dashboard.html',
+    //     controller: 'questionController'
+    // })
+    // .when('/question/:id', {
+    //     templateUrl: 'partials/question.html',
+    //     controller: 'questionController'
+    // })
+    // .when('/add_question',{
+    //     templateUrl: 'partials/add.html',
+    //     controller: 'questionController'
+    // })
+    // .when('/topic/:topic', {
+    //     templateUrl: 'partials/topic.html',
+    //     controller: 'questionController'
+    // })
+    // .when('/user/:userid', {
+    //     templateUrl: 'partials/user.html',
+    //     controller: 'userController'
+    // })
+    // .when('/easter', {
+    //     templateUrl: 'partials/easter.html',
+    //     controller: 'questionController'
+    // })
+    .otherwise({
+        redirectTo: 'partials/home.html'
+    });
+})
+// .directive('navBar', function() {
+//     return {
+//         restrict: 'E',
+//         templateUrl: '/partials/navbar.html'
+//     };
+// })
+// .directive('enavBar', function() {
+//     return {
+//         restrict: 'E',
+//         templateUrl: '/partials/easternav.html'
+//     };
+// })
+// .directive('footer', function() {
+//     return {
+//         restrict: 'E',
+//         templateUrl: '/partials/footer.html'
+//     };
+// })
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express'),
+    app = express(),
+    bp = require('body-parser'),
+    port = 8000,
+    session = require('express-session'),
+    path = require('path');
+
+app.use(bp.json());
+app.use(express.static(path.join(__dirname, './node_modules')));
+app.use(express.static(path.join(__dirname, './client')));
+app.use(session({
+    secret: 'bacon party',
+    resave: false,
+    saveUninitialized: false
+}));
+require(path.join(__dirname, './server/config/mongoose.js'));
+require(path.join(__dirname, './server/config/routes.js'))(app);
+
+
+var server = app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Listening on port ' + port);
+    }
+});
+
+
+// // var io = require('socket.io').listen(server);
+// // var pubsub = require('./server/config/pubsub');
+
+// io.sockets.on('connection', function (socket) {
+// 	console.log( "WE ARE USING SOCKETS!", socket.id );
+
+// 	// console.log(io.sockets);
+
+
+// 	socket.on( "page_load", function ( data ) {
+// 		// console.log( data.user );
+// 		//console.log(socket.id / data.user.name + " navigated to page " + data.location.pathname + data.location.hash );
+// 		// socket.broadcast.emit('reader', data);
+// 		pubsub.publish( socket, data );
+// 	} );
+
+// });
